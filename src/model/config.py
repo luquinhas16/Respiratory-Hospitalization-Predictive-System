45aa9ee469d6f9bc1c4cf7eba030c476dd1e36e7
jvap902 <jvangelopereira@gmail.com>
2026-06-09 16:55:18 -0300

    .gitignore e começo de limpeza para modelo

--- src/model/config.py
+++
@@ -1,5 +0,0 @@
-raw_data_dir = 'dataset'
-
-dataset_dir = 'src/model/dataset'
-
-datasets = ['fepam', 'inmet', 'sus']
--- src/model/dataset/data_cleaning.py
+++ src/model/dataset/data_cleaning.py
@@ -1,23 +1,67 @@
 import pandas as pd
+from tqdm import tqdm
 from src import config
 
 def cleanInmet():
-    raw = pd.read_csv(f'{config.raw_data_dir}/inmet.csv', decimal=',', encoding='latin-1')
-    raw = raw.drop(columns=["Hora UTC"])
+    raw = pd.read_csv(f'{config.raw_data_dir}/inmet.csv')
+    
+    cols_to_remove = ["hora", "pressao_max_hora_ant", "pressao_min_hora_ant", "temperatura_orvalho_max_hora_ant", "temperatura_orvalho_min_hora_ant", "umidade_rel_max_hora_ant", "umidade_rel_min_hora_ant"]
 
-    print(raw)
+    raw = raw.drop(columns=cols_to_remove)
+    
+    clean_cols = raw.columns.to_list() + ["temperatura_min", "temperatura_max"]
+    
+    dataset = pd.DataFrame(columns=clean_cols)
 
-    dataset = pd.DataFrame(columns=raw.columns)
+    dates = list(dict.fromkeys(raw['data'].to_list()))
 
-    avg = min_ = max_ = []
-
-    for date in raw['Data'].to_list():
-        avg = raw[raw['Data'] == date].mean()
-        dataset.loc[len(dataset)] = avg
-        print(dataset)
-        raise
+    for date in tqdm(dates):
+        
+        day_df = raw[raw['data'] == date]
+        
+        cols_avg = day_df.mean(numeric_only=True)
+        min_temp = day_df['temperatura_max_hora_ant'].min()
+        max_temp = day_df['temperatura_min_hora_ant'].max()
+        
+        new_row = [date] + cols_avg.to_list() + [min_temp, max_temp]
+        
+        dataset.loc[len(dataset)] = new_row
+    
+    dataset = dataset.drop(columns=["temperatura_max_hora_ant", "temperatura_min_hora_ant"])
+    print(dataset)
+    
+    dataset.to_csv(f'{config.model_dataset_dir}/inmet.csv')
 
 def cleanFepam():
+    raw = pd.read_csv(f'{config.raw_data_dir}/fepam.csv')
+    
+    raw['data'] = pd.to_datetime(raw['data'])
+    raw['data'] = raw['data'].dt.date
+    
+    min_cols = [f'{col}_min' for col in raw.columns[1:]]
+    max_cols = [f'{col}_max' for col in raw.columns[1:]]
+    
+    clean_cols = raw.columns.to_list() + min_cols + max_cols
+    
+    dataset = pd.DataFrame(columns=clean_cols)
+    
+    dates = list(dict.fromkeys(raw['data'].to_list()))
+    
+    for date in tqdm(dates):
+        day_df = raw[raw['data'] == date]
+        
+        cols_avg = day_df.mean(numeric_only=True).to_list()
+        cols_min = day_df.min(numeric_only=True).to_list()
+        cols_max = day_df.max(numeric_only=True).to_list()
+        
+        new_row = [date] + cols_avg + cols_min + cols_max
+        
+        dataset.loc[len(dataset)] = new_row
+    
+    print(dataset)
+    
+    dataset.to_csv(f'{config.model_dataset_dir}/fepam.csv')
+    
     raise NotImplementedError
 
 def cleanSus():
@@ -25,6 +69,6 @@ def cleanSus():
     
 
 if __name__ == "__main__":
-    cleanInmet()
-    cleanFepam()
+    #cleanInmet()
+    #cleanFepam()
     cleanSus()
